a76e41dac59491b4fbbc2a95f60e0121ee394f02
MikhalGarbuz <garbuzmikhal@gmail.com>
2023-12-15 15:42:20 +0200

    begin to use a chart.js

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ var data = {
 
 // Chart configuration
 var options = {
-  cutoutPercentage: 50,
+  cutout: 120,
   responsive: true,
   maintainAspectRatio: false,
   onAnimationComplete: function () {
@@ -27,12 +27,40 @@ var options = {
   }
 };
 
+var a = 16 +"Volvo";
+console.log(a);
+//centerText plugins
+const centerText = {
+  id: 'CenterText',
+  afterDatasetsDraw(chart){
+    const { ctx }=chart;
+
+    ctx.save();
+    const x = chart.getDatasetMeta(0).data[0].x;
+    const y = chart.getDatasetMeta(0).data[0].y;
+    ctx.textAlign='center';
+    ctx.font = 'bold 30px sans-serif'
+    ctx.fillText("- 6567 usd", x, y)
+
+  }
+}
 // Get the canvas element
 var ctx = document.getElementById('myDoughnutChart').getContext('2d');
+
+
 
 // Create a doughnut chart
 var myDoughnutChart = new Chart(ctx, {
   type: 'doughnut',
   data: data,
-  options: options
+  options: options,
+  plugins: [centerText],
+});
+
+
+var ctx = document.getElementById('myLineChart').getContext('2d');
+
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
 });
